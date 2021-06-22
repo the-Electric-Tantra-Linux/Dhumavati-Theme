99b930e3ecb865ecbbbfe8470b619fe68ebabbd6
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-06-22 15:29:10 -0700

    gnomeshell

--- src/gtk-2.0/render-assets.sh
+++ src/gtk-2.0/render-assets.sh
@@ -1,19 +1,15 @@
 #! /bin/bash
 
 INKSCAPE="/usr/bin/inkscape"
-OPTIPNG="/usr/bin/optipng"
+ZOPFLIPNG="/usr/bin/zopflipng"
+SRC_PATH="./src/retrowave/gtk-2.0"
 
 SRC_FILE="assets.svg"
 ASSETS_DIR="assets"
-
-DARK_SRC_FILE="assets-dark.svg"
-DARK_ASSETS_DIR="assets-dark"
-
 INDEX="assets.txt"
 
 for i in `cat $INDEX`
-do
-
+do 
 if [ -f $ASSETS_DIR/$i.png ]; then
     echo $ASSETS_DIR/$i.png exists.
 else
@@ -21,20 +17,11 @@ else
     echo Rendering $ASSETS_DIR/$i.png
     $INKSCAPE --export-id=$i \
               --export-id-only \
-              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
-    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
-fi
-
-if [ -f $DARK_ASSETS_DIR/$i.png ]; then
-    echo $DARK_ASSETS_DIR/$i.png exists.
-else
-    echo
-    echo Rendering $DARK_ASSETS_DIR/$i.png
-    $INKSCAPE --export-id=$i \
-              --export-id-only \
-              --export-png=$DARK_ASSETS_DIR/$i.png $DARK_SRC_FILE >/dev/null \
-    && $OPTIPNG -o7 --quiet $DARK_ASSETS_DIR/$i.png 
+              --export-background-opacity=0 \
+              --export-type="png" \
+              --export-filename $ASSETS_DIR/$i.png \
+              $SRC_FILE >/dev/null \
+    && $ZOPFLIPNG -ym $ASSETS_DIR/$i.png $ASSETS_DIR/$i.png
 fi
 done
-
 exit 0
--- src/gtk-3.0/render-assets.sh
+++ src/gtk-3.0/render-assets.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 INKSCAPE="/usr/bin/inkscape"
-OPTIPNG="/usr/bin/optipng"
+ZOPFLIPNG="/usr/bin/zopflipng"
 
-INDEX="assets.txt"
-ASSETS_DIR="assets"
 SRC_FILE="assets.svg"
+ASSETS_DIR="assets"
+INDEX="assets.txt"
 
 for i in `cat $INDEX`
 do 
@@ -16,20 +16,11 @@ else
     echo Rendering $ASSETS_DIR/$i.png
     $INKSCAPE --export-id=$i \
               --export-id-only \
-              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
-    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
+              --export-background-opacity=0 \
+              --export-type="png" \
+              --export-filename $ASSETS_DIR/$i.png \
+              $SRC_FILE >/dev/null \
+    && $ZOPFLIPNG -ym $ASSETS_DIR/$i.png $ASSETS_DIR/$i.png
 fi
-if [ -f $ASSETS_DIR/$i@2.png ]; then
-    echo $ASSETS_DIR/$i@2.png exists.
-else
-    echo
-    echo Rendering $ASSETS_DIR/$i@2.png
-    $INKSCAPE --export-id=$i \
-              --export-dpi=180 \
-              --export-id-only \
-              --export-png=$ASSETS_DIR/$i@2.png $SRC_FILE >/dev/null \
-    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i@2.png 
-fi
-
 done
 exit 0
